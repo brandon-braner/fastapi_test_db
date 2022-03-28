dd3e8adf1e1166dc59be8974b279fbfa8c308bc3
Brandon Braner <brandon.braner@gmail.com>
2022-03-27 22:47:25 -0500

    image build on push

--- app/services/transactions/routes.py
+++ app/services/transactions/routes.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter
+from typing import List
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.services.db import get_db
+from app.services.transactions.models import Transactions
+from app.services.transactions.schemas import TransactionsSchema
 
 router = APIRouter(prefix="/transactions")
 
 
 @router.get("/")
-def transactions():
-    return {"message": "Hello transactions"}
+async def transactions(db: Session = Depends(get_db)):
+    transactions = db.query(Transactions).all()
+    return transactions
--- app/services/transactions/schemas.py
+++ app/services/transactions/schemas.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 
 
-class Transactions(BaseModel):
+class TransactionsSchema(BaseModel):
     amount: float
     tax: float
     total: float
